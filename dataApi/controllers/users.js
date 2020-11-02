898c8cbf6791805cfbc6e7195fad6972716c2b6e
a82298 <lguilhermem@hotmail.com>
2020-11-02 17:37:50 +0000

    User

--- dataApi/controllers/users.js
+++ dataApi/controllers/users.js
@@ -1,4 +1,5 @@
 var sql = require('../models/db.js');
+var bcrypt = require('bcryptjs')
 var User = function(user){
     this.iduser = user.iduser;
     this.username = user.username;
@@ -10,9 +11,9 @@ var User = function(user){
     this.followings = user.followings;
 };
 
-User.getUser = function (username) {    
+User.getUser = function (id) {    
     return new Promise(function(resolve, reject) {
-    sql.query("Select iduser, username, birthdate, email, name, followers, following from user where username = ?;", username, function (err, res) {
+    sql.query("Select iduser, username, birthdate, email, name, followers, following from user where iduser = ?;", id, function (err, res) {
             
             if(err) {
                 console.log("error: ", err);
@@ -24,5 +25,183 @@ User.getUser = function (username) {
         });   
     })       
 };
+
+User.getBetsFromUser = function (iduser) {    
+    return new Promise(function(resolve, reject) {
+    sql.query("Select * from bet where iduser = ?;", iduser, function (err, res) {
+            
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });   
+    })       
+};
+
+User.getPostsFromUser = function (iduser) {    
+    return new Promise(function(resolve, reject) {
+    sql.query("Select * from post where iduser = ? and idgroup = 1;", iduser, function (err, res) {
+            
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });   
+    })       
+};
+
+User.getFollowersFromUser = function (iduser) {    
+    return new Promise(function(resolve, reject) {
+    sql.query("Select * from follower where following = ?;", iduser, function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });   
+    })       
+};
+
+User.getFollowingFromUser = function (iduser) {    
+    return new Promise(function(resolve, reject) {
+    sql.query("Select * from follower where me = ?;", iduser, function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });   
+    })       
+};
+
+User.getPublicPostsFromUser = function (iduser) {    
+    return new Promise(function(resolve, reject) {
+    sql.query("Select * from post where iduser = ? and public = true;", iduser, function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });   
+    })       
+};
+
+User.createUser = function (user) {    
+    var newPassword = bcrypt.hashSync(password, 10);
+    var parameters = [user.username, user.birthdate, newPassword, user.email, user.name]
+    return new Promise(function(resolve, reject) {
+    sql.query("INSERT INTO user (username, birthdate, password, email, name, followers, following) values (?, ?, ?, ?, ?, 0, 0)", parameters, function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                console.log(res.insertId);
+                resolve(res);
+            }
+        });   
+    })       
+};
+
+User.login = function (username) {    
+    return new Promise(function(resolve, reject) {
+    sql.query("Select iduser, password from user where username = ?;", username, function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });   
+    })       
+};
+
+User.createFollower = function (following, me) {    
+    return new Promise(function(resolve, reject) {
+    sql.query("INSERT INTO follower (me, following) values (?, ?)", [me, following], function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                console.log(res.insertId);
+                resolve(res);
+            }
+        });   
+    })       
+};
+
+User.updateUser = function (id, user) {
+    var parameters = [user.username, user.birthdate, user.email, user.name, id]    
+    return new Promise(function(resolve, reject) {
+    sql.query("UPDATE user SET username = ?, birthdate = ?, email = ?, name = ? WHERE iduser = ?", parameters, function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });   
+    })       
+};
+
+User.updatePassword = function (id, password) {
+    var newPassword = bcrypt.hashSync(password, 10);
+    return new Promise(function(resolve, reject) {
+    sql.query("UPDATE user SET passaword = ? WHERE iduser = ?", [newPassword, id], function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });   
+    })       
+};
+
+User.deleteUser = function (id){
+    return new Promise(function(resolve, reject) {
+        sql.query("DELETE FROM user WHERE iduser = ?", id, function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+            
+                resolve(res);
+            }
+        });   
+    })
+}
+
+User.deleteFollower = function (following, me){
+    return new Promise(function(resolve, reject) {
+        sql.query("DELETE FROM follower WHERE following = ? and me = ?", [following, me], function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+            
+                resolve(res);
+            }
+        });   
+    })
+}
 
 module.exports= User;
--- dataApi/routes/users.js
+++ dataApi/routes/users.js
@@ -1,6 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcryptjs')
 var Users = require('../controllers/users')
+
+/*
+-> Users (Luisinho)
+
+      -> GET
+
+      -> POST
+
+      -> PUT
+
+	- users/:id (Update de um utilizador)
+
+	- users/:id/password (Update da password de um utilizador)
+
+      -> DELETE
+	
+	- users/:id (Remoção de um utilizador)
+
+	- users/:id/followers (Remoção de um seguidor)
+
+*/
+
 
 /* GET an user. */
 router.get('/:id', function(req, res, next) {
@@ -14,10 +37,159 @@ router.get('/:id', function(req, res, next) {
         })
 });
 
-/* GET an user. */
-router.get('/:id/posts', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+/* GET user bets. */
+router.get('/:id/bets', function(req, res, next) {
+    Users.getBetsFromUser(req.params.id)
+         .then(dados => {
+             res.jsonp(dados)
+         })
+         .catch(erro => {
+             res.status(500).jsonp(erro)
+         })
   });
+
+/* GET user posts. */
+router.get('/:id/posts', function(req, res, next) {
+    Users.getPostsFromUser(req.params.id)
+         .then(dados => {
+            res.jsonp(dados)
+         })
+         .catch(erro => {
+            res.status(500).jsonp(erro)
+         })
+  });
+
+/* GET user followers. */
+router.get('/:id/followers', function(req, res, next) {
+    Users.getFollowersFromUser(req.params.id)
+         .then(dados => {
+            res.jsonp(dados)
+         })
+         .catch(erro => {
+            res.status(500).jsonp(erro)
+         })
+  });
+
+/* GET user following. */
+router.get('/:id/following', function(req, res, next) {
+    Users.getFollowingFromUser(req.params.id)
+         .then(dados => {
+            res.jsonp(dados)
+         })
+         .catch(erro => {
+            res.status(500).jsonp(erro)
+         })
+  });
+
+/* GET user public posts. */
+router.get('/:id/posts/publics', function(req, res, next) {
+    Users.getPublicPostsFromUser(req.params.id)
+         .then(dados => {
+            res.jsonp(dados)
+         })
+         .catch(erro => {
+            res.status(500).jsonp(erro)
+         })
+  });
+
+
+/* POST new user. */
+router.post('/', function(req, res, next) {
+  Users.createUser(req.body)
+        .then(dados => {
+            res.jsonp(dados)
+        })
+        .catch(erro => {
+            res.status(500).jsonp(erro)
+        })
+})
+
+/* POST user login. */
+router.post('/login', function(req, res, next) {
+    var user = req.body
+    Users.login(req.body)
+       .then(dados => {
+            var result = {
+                login : false,
+                message : "Utilizador não existe."
+            }
+            if(dados.length != 0){
+                if(bcrypt.compareSync(user.password, dados[0].password)){
+                    result.login = true
+                    result.message = "Credenciais corretas."
+                    result.utilizador = dados[0]
+                    res.jsonp(result)
+                }
+                else{
+                    result.message = "Password errada."
+                    res.jsonp(result)
+                }
+            }
+            else {res.jsonp(result)}
+            
+       })
+       .catch(erro => {
+           console.log(erro); 
+           res.status(500).jsonp(erro)
+        })
+});
+
+/* POST new user. */
+router.post('/:id/followers', function(req, res, next) {
+    Users.createFollower(req.params.id, req.body.id)
+          .then(dados => {
+              res.jsonp(dados)
+          })
+          .catch(erro => {
+              res.status(500).jsonp(erro)
+          })
+})
+
+/* PUT Update user. */
+router.put('/:id', function(req, res, next) {
+    Users.updateUser(req.params.id, req.body)
+          .then(dados => {
+              res.jsonp(dados)
+          })
+          .catch(erro => {
+              res.status(500).jsonp(erro)
+          })
+})
+
+/* PUT Update user password. */
+router.put('/:id/password', function(req, res, next) {
+    Users.updatePassword(req.params.id, req.body.password)
+          .then(dados => {
+              res.jsonp(dados)
+          })
+          .catch(erro => {
+              res.status(500).jsonp(erro)
+          })
+})
+
+/* DELETE Delete user. */
+router.delete('/:id', function(req, res, next) {
+    Users.deleteUser(req.params.id)
+          .then(dados => {
+              res.jsonp(dados)
+          })
+          .catch(erro => {
+              res.status(500).jsonp(erro)
+          })
+})
+
+/* DELETE Delete user. */
+router.delete('/:id/followers', function(req, res, next) {
+    Users.deleteUser(req.params.id, req.body.id)
+          .then(dados => {
+              res.jsonp(dados)
+          })
+          .catch(erro => {
+              res.status(500).jsonp(erro)
+          })
+})
+
+
 
 // Get router.get(rota, function(req, res, next){});
 // Post router.post(rota, function(req, res, next){});
