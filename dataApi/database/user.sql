cc49361f3b3564900e4f3df7710f0845198580f3
a82298 <lguilhermem@hotmail.com>
2020-11-02 14:20:44 +0000

    User for mysql

---
+++ dataApi/database/user.sql
@@ -0,0 +1,3 @@
+CREATE USER 'bettingspree'@'localhost' IDENTIFIED BY 'PEI2020';
+GRANT ALL PRIVILEGES ON * . * TO 'bettingspree'@'localhost';
+FLUSH PRIVILEGES;
--- dataApi/models/db.js
+++ dataApi/models/db.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql')
 var connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
-  password: 'root',
+  user: 'bettingspree',
+  password: 'PEI2020',
   database: 'databettingspree'
 })
 
