0f06d0522117e22603e3fc2cbab26a60ee90a508
Filipe <filipe-cunha1@hotmail.com>
2020-11-03 16:28:13 +0000

    interface toolbar inicio

--- interface/src/router/index.js
+++ interface/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import AuthApp from '../views/AuthApp.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/feed',
+    name: 'Feed',
+    component: AuthApp
   },
   {
     path: '/about',
--- interface/src/views/Login.vue
+++ interface/src/views/Login.vue
@@ -24,6 +24,58 @@
 
 
 
+
+        <v-container>
+                    <v-btn icon>
+                <v-icon>mdi-home</v-icon>
+            </v-btn>
+            </v-container>
+
+<v-container>
+                    <v-btn icon>
+                <v-icon>mdi-home</v-icon>
+            </v-btn>
+            </v-container>
+            <v-container>
+                <span>
+            <a> Jogos </a>
+                </span>
+                </v-container>
+                <v-container>
+               <span>
+            <a> Suas Apostas </a>
+              </span>
+              </v-container>
+              <v-container>
+              <span>
+            <a> Seus Rascunhos </a>
+            </span>
+            </v-container>
+            <v-container>
+            <v-btn > Carregar Dinheiro </v-btn>
+            </v-container>
+            <v-container>
+            <v-btn > Personal Score </v-btn>
+            </v-container>
+            <v-spacer>
+            <v-toolbar-title>Luizz</v-toolbar-title>
+            <v-toolbar-title>100000$</v-toolbar-title>
+            </v-spacer>
+            <container class="pl-5">
+            <v-btn icon>
+                <v-icon>mdi-account</v-icon>
+            </v-btn>
+            <!--
+                <v-avatar
+                                    slot="offset"
+                                    class="mx-auto d-block"
+                                >
+                                <v-icon>mdi-account</v-icon>
+                </v-avatar>
+                -->
+            </container>
+
+
 <script>
 import axios from "axios"
 //const h = require("@/config/hosts").hostAPI
@@ -57,6 +109,7 @@ import VueJwtDecode from "vue-jwt-decode";
           this.$emit("refreshLogout")
           this.$router.push("/universidade");
        }*/
+          this.$emit("refreshLogout")
        } 
        catch (err) {
         this.password = ""
