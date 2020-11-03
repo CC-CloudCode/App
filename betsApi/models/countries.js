49eff5f4f86acd06fbdbb78fc596454e2f96ff80
Luís Braga <a82088@alunos.uminho.pt>
2020-11-03 15:38:32 +0000

    countries

---
+++ betsApi/models/countries.js
@@ -0,0 +1,10 @@
+var sql = require('./db.js');
+
+var Country = function(v){
+    this.idcountry = v.idcountry;
+    this.name = v.name;
+    this.flag = v.flag;
+    this.code = v.code;
+};
+
+module.exports= Country;
