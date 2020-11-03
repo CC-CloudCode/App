8c1ad221f22992698c426aff9e418f329315caf1
Mateus Silva <36518616+mateussilva98@users.noreply.github.com>
2020-11-03 15:37:40 +0000

    arualização modelo bets

---
+++ betsApi/database/country_teste_povoamento.sql
@@ -0,0 +1,5 @@
+insert into country values (1, 'EN', "England", "https://media.api-sports.io/flags/gb.svg");
+insert into country values (2, 'ES', "Spain", "https://media.api-sports.io/flags/es.svg");
+insert into country values (3, 'PT', "Portugal", "https://media.api-sports.io/flags/pt.svg");
+
+select * from country;
