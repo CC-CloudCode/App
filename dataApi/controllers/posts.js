d1fd4c2abf34226a2f193d55701ad43a55e578aa
47Balboa <shahzod1997@hotmail.com>
2020-11-04 16:21:45 -0500

    rotas Posts

--- dataApi/controllers/posts.js
+++ dataApi/controllers/posts.js
@@ -1,10 +1,148 @@
 var sql = require('../models/db.js');
-var Post = function(pedreste){
-    this.idPedestre = pedreste.idPedestre;
-    this.latitude = pedreste.latitude;
-    this.longitude = pedreste.longitude;
-    this.idPassadeira = pedreste.idPassadeira;
+var Post = function(post){
+    this.idPost = post.idPost;
+    this.text = post.text;
+    this.public = post.public;
+    this.date = post.date;
+    this.iduser = post.iduser;
+    this.idbet = post.idbet;
+    this.betpublic = post.betpublic;
+    this.idgroup = post.idgroup;
+
 };
+Post.getPosts = function (idPost) {
+    return new Promise(function(resolve, reject) {
+    sql.query("Select *from post", function (err, res) {
+
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });
+    })
+};
+
+Post.getPost = function (idPost) {
+    return new Promise(function(resolve, reject) {
+    sql.query("Select idPost, text, public, date, iduser, idbet, betpublic,idgroup from post where idPost = ?;", idPost, function (err, res) {
+
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });
+    })
+};
+
+Post.getPostComments = function (idPost) {
+    return new Promise(function(resolve, reject) {
+    sql.query("Select *from comment where idPost = ?;", idPost, function (err, res) {
+
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });
+    })
+};
+
+Post.getPostUpvotes = function (idPost) {
+    return new Promise(function(resolve, reject) {
+    sql.query("Select *from upvotes where idPost = ?;", idPost, function (err, res) {
+
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });
+    })
+};
+
+Post.createPost = function (post) {
+    var parameters = [post.text, post.public, post.date, post.iduser, post.idbet,post.betpublic ,post.idgroup]
+    return new Promise(function(resolve, reject) {
+    sql.query("INSERT INTO post (text, public, date, iduser, idbet, betpublic, idgroup) values (?, ?, ?, ?, ?, ?, ?)", parameters, function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                console.log(res.insertId);
+                resolve(res);
+            }
+        });
+    })
+};
+
+Post.createUpvotes = function (post) {
+    return new Promise(function(resolve, reject) {
+    sql.query("INSERT INTO upvotes (iduser, idpost) values (?, ?)", [post.iduser,post.idpost], function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                console.log(res.insertId);
+                resolve(res);
+            }
+        });
+    })
+};
+
+Post.updatePost = function (id, post) {
+    var parameters = [post.text, post.public, post.date,  id]
+    return new Promise(function(resolve, reject) {
+    sql.query("UPDATE post SET text = ?, public = ?, date = ? WHERE idpost = ?", parameters, function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });
+    })
+};
+
+Post.deletePost = function (id){
+    return new Promise(function(resolve, reject) {
+        sql.query("DELETE post FROM post INNER JOIN upvotes ON post.idpost = ?", id, function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });
+    })
+}
+
+Post.deleteUpvote = function (id){
+    return new Promise(function(resolve, reject) {
+        sql.query("DELETE upvotes FROM upvotes INNER JOIN  post WHERE idpost = ?", id, function (err, res) {
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });
+    })
+}
 
 
 module.exports= Post;
--- dataApi/routes/posts.js
+++ dataApi/routes/posts.js
@@ -2,9 +2,142 @@ var express = require('express');
 var router = express.Router();
 var Posts = require('../controllers/posts')
 
-/* GET home page. */
+/*
+-> Posts (Zod)
+
+     -> GET:
+
+
+  - posts/ (Todos os posts)
+
+	- posts/:id (Toda a informação relativa a um post)
+
+	- posts/:id/comments (Todos os comentários de um post)
+
+	- posts/:id/upvotes (Todos os utilizadores que deram upvote num post)
+
+      -> POST:
+
+         - posts/ (Inserção de um post)
+
+	 - posts/:id/upvotes (Inserção de um upvote)
+
+      -> PUT:
+
+	 - posts/:id (Update de um post)
+
+      -> DELETE:
+
+	 - posts/:id (Eliminação de um post)
+
+	 - posts/upvotes/:id (Eliminação de um upvote)
+
+   */
+
+/* GET Posts */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Posts.getPosts(req.params.id)
+     .then(dados => {
+          res.jsonp(dados)
+     })
+     .catch(erro => {
+         console.log(erro);
+         res.status(500).jsonp(erro)
+      })
 });
+
+/* GET Post */
+router.get('/:id', function(req, res, next) {
+  Posts.getPost(req.params.id)
+     .then(dados => {
+          res.jsonp(dados)
+     })
+     .catch(erro => {
+         console.log(erro);
+         res.status(500).jsonp(erro)
+      })
+});
+
+/* GET all comments from a specific Post */
+router.get('/:id/comments', function(req, res, next) {
+  Posts.getPostComments(req.params.id)
+     .then(dados => {
+          res.jsonp(dados)
+     })
+     .catch(erro => {
+         console.log(erro);
+         res.status(500).jsonp(erro)
+      })
+});
+
+/* GET all upvotes from a specific Post */
+router.get('/:id/upvotes', function(req, res, next) {
+  Posts.getPostUpvotes(req.params.id)
+     .then(dados => {
+          console.log(req.body)
+          res.jsonp(dados)
+     })
+     .catch(erro => {
+         //console.log(erro);
+         res.status(500).jsonp(erro)
+      })
+});
+
+
+/* POST new Post. */
+router.post('/', function(req, res, next) {
+  Posts.createPost(req.body)
+        .then(dados => {
+            res.jsonp(dados)
+        })
+        .catch(erro => {
+            res.status(500).jsonp(erro)
+        })
+})
+
+
+/* POST upvotes into specific post. */
+router.post('/:id/upvotes', function(req, res, next) {
+  Posts.createUpvotes(req.body)
+        .then(dados => {
+            res.jsonp(dados)
+        })
+        .catch(erro => {
+            res.status(500).jsonp(erro)
+        })
+})
+
+/* PUT Update Post. */
+router.put('/:id', function(req, res, next) {
+  Posts.updatePost(req.params.id,req.body)
+        .then(dados => {
+            res.jsonp(dados)
+        })
+        .catch(erro => {
+            res.status(500).jsonp(erro)
+        })
+})
+
+/* DELETE a specific Post. */
+router.delete('/:id', function(req, res, next) {
+  Posts.deletePost(req.params.id)
+        .then(dados => {
+            res.jsonp(dados)
+        })
+        .catch(erro => {
+            res.status(500).jsonp(erro)
+        })
+})
+
+/* DELETE Upvote from specific post. */
+router.delete('/:id/upvotes/:idupvote', function(req, res, next) {
+  Posts.deleteUpvote(req.params.id, req.body.idupvote)
+        .then(dados => {
+            res.jsonp(dados)
+        })
+        .catch(erro => {
+            res.status(500).jsonp(erro)
+        })
+})
 
 module.exports = router;
