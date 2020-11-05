43f44fc331e5956672e7ba70a95cdf6e6eb6a0ae
AdrianaMeireles80 <adriana_1998@live.com.pt>
2020-11-05 15:47:41 +0000

    bets

--- dataApi/controllers/bets.js
+++ dataApi/controllers/bets.js
@@ -1,9 +1,99 @@
 var sql = require('../models/db.js');
-var Bet = function(pedreste){
-    this.idPedestre = pedreste.idPedestre;
-    this.latitude = pedreste.latitude;
-    this.longitude = pedreste.longitude;
-    this.idPassadeira = pedreste.idPassadeira;
+var Bet = function(b){
+    this.idbet = b.idbet;
+    this.date = b.date;
+    this.money = b.money;
+    this.iduser = b.iduser;
+    this.state = b.state;
+    this.originalbetid = b.originalbetid;
+};
+
+// Informação relativa a uma aposta
+Bet.getBet = function (id) {    
+    return new Promise(function(resolve, reject) {
+    sql.query("Select * from bet where idbet = ?;", id, function (err, res) {
+            
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });   
+    })       
+};
+
+//Todos os jogos e resultados apostados da aposta
+Bet.getEventsFromBets = function (idbet) {    
+    return new Promise(function(resolve, reject) {
+        //state:
+        //0- não terminado
+        //1- terminado
+        //2- pendente
+
+    sql.query("Select * from event where idbet= ? and state = 1;", idbet, function (err, res) {
+            
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });   
+    })       
+};
+
+Bet.getUsersFromBets = function (idbet) {    
+    return new Promise(function(resolve, reject) {
+        //originalbetid: (porque é que não é booleano ?)
+        // 0- não foi copiado
+        //1- foi copiado
+
+    sql.query("Select bet.iduser , user.name from bet inner join user on bet.iduser = user.iduser  where idbet = ? and originalbetid = 1;", idbet, function (err, res) {
+            
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }
+            else{
+                resolve(res);
+            }
+        });   
+    })       
+};
+
+Bet.createBet = function (bet){
+    var parameters = [bet.date, bet.money, bet.iduser,bet.originalbetid]
+    return new Promise(function(resolve,reject){
+    sql.query("INSERT INTO bet (date,money,iduser,state,originalbetid) values(?,?,?,2,?)",parameters,function(err,res){
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }   
+            else{
+                console.log(res.insertId);
+                resolve(res);
+            }
+        });
+    })
+};
+
+Bet.createEvent = function (idbetapi,odd,bettype,idbet){ 
+    
+    return new Promise(function(resolve,reject){
+    sql.query("INSERT INTO event (idbetapi,odd,bettype,idbet,state) values(?,?,?,?,1)",[idbetapi,odd,bettype,idbet],function(err,res){
+            if(err) {
+                console.log("error: ", err);
+                reject(err);
+            }   
+            else{
+                console.log(res.insertId);
+                resolve(res);
+            }
+        });
+    })
 };
 
 
--- dataApi/routes/bets.js
+++ dataApi/routes/bets.js
@@ -2,9 +2,62 @@ var express = require('express');
 var router = express.Router();
 var Bets = require('../controllers/bets')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET bets. */
+router.get('/:id', function(req, res, next) {
+  Bets.getBet(req.params.id)
+     .then(dados => {
+          res.jsonp(dados)
+     })
+     .catch(erro => {
+         console.log(erro); 
+         res.status(500).jsonp(erro)
+      })
 });
+
+/*GET evento*/
+
+router.get('/:id/events', function(req, res, next) {
+  Bets.getEventsFromBets(req.params.id)
+       .then(dados => {
+          res.jsonp(dados)
+       })
+       .catch(erro => {
+          res.status(500).jsonp(erro)
+       })
+});
+
+/*GET copies*/
+router.get('/:id/copies', function(req, res, next) {
+  Bets.getUsersFromBets(req.params.id)
+       .then(dados => {
+          res.jsonp(dados)
+       })
+       .catch(erro => {
+          res.status(500).jsonp(erro)
+       })
+});
+
+/*POST bet */
+router.post('/', function(req, res, next) {
+  Bets.createBet(req.body)
+        .then(dados => {
+            res.jsonp(dados)
+        })
+        .catch(erro => {
+            res.status(500).jsonp(erro)
+        })
+})
+
+/*POST event*/
+router.post('/:id/events', function(req, res, next) {
+  Bets.createEvent(req.params.id, req.body.id)
+        .then(dados => {
+            res.jsonp(dados)
+        })
+        .catch(erro => {
+            console.log(erro);
+            res.status(500).jsonp(erro)
+        })
+})
 
 module.exports = router;
