e330d1249b4006cf0a4d51ebc6e723ac0ff53f0d
a82298 <lguilhermem@hotmail.com>
2020-11-07 15:13:58 +0000

    Divisão interface

---
+++ dataApi/database/triggers.sql
@@ -0,0 +1,22 @@
+Drop Trigger IF EXISTS follower_insert;
+Delimiter $$
+CREATE TRIGGER follower_insert
+AFTER INSERT ON databettingspree.follower 
+FOR EACH ROW
+BEGIN
+Update databettingspree.user Set followers = followers + 1 where iduser = NEW.following;
+Update databettingspree.user Set following = following + 1 where iduser = NEW.me;
+END $$
+Delimiter ;
+
+Drop Trigger IF EXISTS follower_delete;
+Delimiter $$
+CREATE TRIGGER follower_delete
+AFTER DELETE ON databettingspree.follower 
+FOR EACH ROW
+BEGIN
+Update databettingspree.user Set followers = followers - 1 where iduser = Old.following;
+Update databettingspree.user Set following = following - 1 where iduser = Old.me;
+END $$
+Delimiter ;
+
