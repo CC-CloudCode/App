779c4f3d8a09ebf433627cdb2a378fd6c892bbe5
MelhorQueSVN <a82300@alunos.uminho.pt>
2020-11-29 19:28:41 +0000

    Rework interface bets (datatable)

---
+++ interface/src/components/H2h.vue
@@ -0,0 +1,43 @@
+<template>
+                            <v-simple-table dense fixed-header height="300px">
+                              <template v-slot:default>
+                                <thead>
+                                  <tr>
+                                    <th class="text-left">
+                                      Data
+                                    </th>
+                                    <th class="text-left">
+                                      Equipa Casa
+                                    </th>
+                                    <th class="text-left">
+                                      Resultado
+                                    </th>
+                                    <th class="text-left">
+                                      Equipa Fora
+                                    </th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr v-for="item in h2h" :key="item.idfixture">
+                                    <td>{{ item.date.substring(0, 10) }}</td>
+                                    <td v-if="id_home_team == item.homeTeamId" >
+                                      {{ statshometeam }}
+                                    </td>
+                                    <td v-else>{{ statsawayteam }}</td>
+                                    <td>{{ item.score }}</td>
+                                    <td v-if="id_away_team == item.awayTeamId">
+                                      {{ statsawayteam }}
+                                    </td>
+                                    <td v-else>{{ statshometeam }}</td>
+                                  </tr>
+                                </tbody>
+                              </template>
+                            </v-simple-table>
+</template>
+
+<script> 
+   export default { 
+        
+        props: ['h2h','id_home_team','id_away_team', 'statshometeam', 'statsawayteam'], 
+   }
+</script>
---
+++ interface/src/components/Standings.vue
@@ -0,0 +1,197 @@
+<template> 
+                            <v-simple-table dense fixed-header height="300px">
+                              <template v-slot:default>
+                                <thead>
+                                   <tr>
+                                    
+                                    <th class="text-left"></th>
+                                    <th class="text-left">
+                                      Equipa
+                                    </th>
+                                    <th class="text-left">
+                                      Posição
+                                    </th>
+                                    <th class="text-left">
+                                      Pontos
+                                    </th>
+                                    <th class="text-left"> 
+                                      Jogos Jogados
+                                    </th> 
+                                    <th class="text-left"> 
+                                      Vitórias
+                                    </th>
+                                    <th class="text-left"> 
+                                      Empates
+                                    </th> 
+                                    <th class="text-left"> 
+                                      Derrotas
+                                    </th>
+                                    <th class="text-left"> 
+                                      Golos Marcados
+                                    </th> 
+                                    <th class="text-left"> 
+                                      Golos Sofridos 
+                                    </th> 
+                                    <th class="text-left"> 
+                                      Diferença de Golos 
+                                    </th> 
+
+
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  
+                                  <tr
+                                    v-for="item in standings"
+                                    :key="item.teamname"
+                                  >
+                                    <td
+                                      :style="{
+                                        backgroundColor:
+                                          item.teamname == statshometeam ||
+                                          item.teamname == statsawayteam
+                                            ? '#afd29a'
+                                            : 'transparent',
+                                      }"
+                                    >
+                                      <v-img
+                                        v-bind:src="item.teamlogo"
+                                        width="20px"
+                                      ></v-img>
+                                    </td>
+                                    
+                                    <td
+                                      :style="{
+                                        backgroundColor:
+                                          item.teamname == statshometeam ||
+                                          item.teamname == statsawayteam
+                                            ? '#afd29a'
+                                            : 'transparent',
+                                      }"
+                                    >
+                                      {{ item.teamname }}
+                                    </td>
+                                    
+                                    <td
+                                      :style="{
+                                        backgroundColor:
+                                          item.teamname == statshometeam ||
+                                          item.teamname == statsawayteam
+                                            ? '#afd29a'
+                                            : 'transparent',
+                                      }"
+                                    >
+                                      {{ item.position }}
+                                    </td>
+                                    
+                                    <td
+                                      :style="{
+                                        backgroundColor:
+                                          item.teamname == statshometeam ||
+                                          item.teamname == statsawayteam
+                                            ? '#afd29a'
+                                            : 'transparent',
+                                      }"
+                                    >
+                                      {{ item.points }}
+                                    </td> 
+
+                                    <td
+                                      :style="{
+                                        backgroundColor:
+                                          item.teamname == statshometeam ||
+                                          item.teamname == statsawayteam
+                                            ? '#afd29a'
+                                            : 'transparent',
+                                      }"
+                                    >
+                                      {{ item.matchsPlayedTotal }}
+                                    </td> 
+
+                                    <td
+                                      :style="{
+                                        backgroundColor:
+                                          item.teamname == statshometeam ||
+                                          item.teamname == statsawayteam
+                                            ? '#afd29a'
+                                            : 'transparent',
+                                      }"
+                                    >
+                                      {{ item.winsTotal }}
+                                    </td> 
+                                    
+                                    <td
+                                      :style="{
+                                        backgroundColor:
+                                          item.teamname == statshometeam ||
+                                          item.teamname == statsawayteam
+                                            ? '#afd29a'
+                                            : 'transparent',
+                                      }"
+                                    >
+                                      {{ item.losesTotal }}
+                                    </td> 
+                                    
+                                    <td
+                                      :style="{
+                                        backgroundColor:
+                                          item.teamname == statshometeam ||
+                                          item.teamname == statsawayteam
+                                            ? '#afd29a'
+                                            : 'transparent',
+                                      }"
+                                    >
+                                      {{ item.drawsTotal }}
+                                    </td> 
+
+                                    <td
+                                      :style="{
+                                        backgroundColor:
+                                          item.teamname == statshometeam ||
+                                          item.teamname == statsawayteam
+                                            ? '#afd29a'
+                                            : 'transparent',
+                                      }"
+                                    >
+                                      {{ item.goalsForTotal }}
+                                    </td> 
+
+                                    <td
+                                      :style="{
+                                        backgroundColor:
+                                          item.teamname == statshometeam ||
+                                          item.teamname == statsawayteam
+                                            ? '#afd29a'
+                                            : 'transparent',
+                                      }"
+                                    >
+                                      {{ item.goalsAgainstTotal }}
+                                    </td> 
+
+                                    <td
+                                      :style="{
+                                        backgroundColor:
+                                          item.teamname == statshometeam ||
+                                          item.teamname == statsawayteam
+                                            ? '#afd29a'
+                                            : 'transparent',
+                                      }"
+                                    >
+                                      {{ item.goalsDiff }}
+                                    </td>
+                                    
+                                    
+
+                                  </tr>
+                                </tbody>
+                              </template>
+                            </v-simple-table>
+
+</template> 
+
+<script> 
+   export default { 
+        
+        props: ['standings','statshometeam','statsawayteam'], 
+   }
+</script>
