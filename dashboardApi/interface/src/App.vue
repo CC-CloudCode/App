7ed538c1d36733a4005d496f68cf80b3fa6f9b63
guiandrade2 <36737704+guiandrade2@users.noreply.github.com>
2021-01-17 22:55:14 +0000

    interface plots

--- dashboardApi/interface/src/App.vue
+++ dashboardApi/interface/src/App.vue
@@ -29,9 +29,6 @@
 
     </v-app-bar>
 
-    <!--<v-main>
-      <line-chart v-if="loaded" :title="title1" :chartdata="chartdata1" />
-    </v-main>-->
     <v-main class="grey lighten-3">
       <v-container>
         <v-row>
@@ -42,7 +39,7 @@
           <v-sheet
               rounded="lg"
             >
-              <line-chart v-if="loaded" :title="title1" :chartdata="chartdata1" />
+              <line-chart v-if="loaded" :title="title1" :chartdata="chartdata1"/>
             </v-sheet>
           
           </v-col>
@@ -54,11 +51,11 @@
            <v-sheet
               rounded="lg"
             >
-            <line-chart v-if="loaded" :title="title2" :chartdata="chartdata2" />
+            <line-chart v-if="loaded" :title="title2" :chartdata="chartdata2"/>
             </v-sheet>
           </v-col>
         </v-row>
-                <v-row>
+        <v-row>
           <v-col
             cols="12"
             sm="6"
@@ -66,7 +63,7 @@
           <v-sheet
               rounded="lg"
             >
-              <line-chart v-if="loaded" :title="title3" :chartdata="chartdata3" />
+              <line-chart v-if="loaded" :title="title3" :chartdata="chartdata3"/>
             </v-sheet>
           
           </v-col>
@@ -78,7 +75,31 @@
            <v-sheet
               rounded="lg"
             >
-            <line-chart v-if="loaded" :title="title4" :chartdata="chartdata4" />
+            <line-chart v-if="loaded" :title="title4" :chartdata="chartdata4"/>
+            </v-sheet>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="6"
+          >
+          <v-sheet
+              rounded="lg"
+            >
+              <line-chart v-if="loaded" :title="title5" :chartdata="chartdata5"/>
+            </v-sheet>
+          
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="6"
+          >
+           <v-sheet
+              rounded="lg"
+            >
+            <ChartTeste v-if="loaded" :title="title6" :titleaux="title7" :chartdata="chartdata6" :chartdataaux="chartdata7"/>
             </v-sheet>
           </v-col>
         </v-row>
@@ -89,10 +110,11 @@
 
 <script>
 import LineChart from './components/Chart.vue';
+import ChartTeste from "./components/ChartTeste"
 import axios from "axios";
 export default {
   name: 'App',
-  components: { LineChart },
+  components: { LineChart, ChartTeste },
   data() {
     return {
       loaded: false,
@@ -104,6 +126,12 @@ export default {
       title3: "Número de posts realizados nos últimos 5 dias",
       chartdata4: null,
       title4: "Número de posts realizados ontem por hora",
+      chartdata5: null,
+      title5: "Número de rascunhos criados nos últimos 5 dias",
+      chartdata6: null,
+      chartdata7: null,
+      title6: "Publicações públicas",
+      title7: "Publicações privadas",
     };
   },
   created() {
@@ -112,14 +140,20 @@ export default {
     let perdidoroute = "http://localhost:4052/bets/dashboard/lostmoney"
     let postcountroute = "http://localhost:4052/posts/postscount"
     let postcounthourroute = "http://localhost:4052/posts/postscounthour"
+    let draflastdaysroute = "http://localhost:4052/bets/dashboard/draftcount"
+    let publicpostcoutroute = "http://localhost:4052/posts/publicpostscount"
+    let privatepostcoutroute = "http://localhost:4052/posts/privatepostscount"
 
     const res_money = axios.get(moneyroute);
     const res_perdido = axios.get(perdidoroute);
     const res_postscount = axios.get(postcountroute);
     const res_postscounthour = axios.get(postcounthourroute);
+    const res_draflastdays = axios.get(draflastdaysroute);
+    const res_publicpostcout = axios.get(publicpostcoutroute);
+    const res_privatepostcout = axios.get(privatepostcoutroute);
 
     axios
-      .all([res_money, res_perdido, res_postscount, res_postscounthour])
+      .all([res_money, res_perdido, res_postscount, res_postscounthour, res_draflastdays, res_publicpostcout, res_privatepostcout])
       .then(
         axios.spread((...responses) => {
           var result = {};
@@ -145,6 +179,25 @@ export default {
             result[responses[3].data[i]["hour(date)"]] = responses[3].data[i]["count(idpost)"];
           }
           this.chartdata4 = result
+
+          result = {};
+          for ( i = 0; i < responses[4].data.length; i++) {
+            result[responses[4].data[i]["date(date)"]] = responses[4].data[i]["count(idbet)"];
+          }
+          this.chartdata5 = result
+
+          result = {};
+          for ( i = 0; i < responses[5].data.length; i++) {
+            result[responses[5].data[i]["date(date)"]] = responses[5].data[i]["count(idpost)"];
+          }
+          this.chartdata6 = result
+
+          result = {};
+          for ( i = 0; i < responses[6].data.length; i++) {
+            result[responses[6].data[i]["date(date)"]] = responses[6].data[i]["count(idpost)"];
+          }
+          this.chartdata7 = result
+
 
           this.loaded=true
 
--- dashboardApi/interface/src/components/ChartTeste.vue
+++ dashboardApi/interface/src/components/ChartTeste.vue
@@ -4,7 +4,8 @@ export default {
   extends: Line,
   data() {
     return {
-      gradient: null
+      gradient: null,
+      gradient2: null
     };
   },
   props: {
@@ -12,7 +13,15 @@ export default {
       type: Object,
       default: null
     },
+    chartdataaux: {
+      type: Object,
+      default: null
+    },
     title: {
+      type: String,
+      default: null
+    },
+    titleaux: {
       type: String,
       default: null
     }
@@ -24,6 +33,12 @@ export default {
     this.gradient.addColorStop(0, "rgba(255, 0,0, 0.5)");
     this.gradient.addColorStop(0.5, "rgba(255, 0, 0, 0.25)");
     this.gradient.addColorStop(1, "rgba(255, 0, 0, 0)");
+    this.gradient2 = this.$refs.canvas
+      .getContext("2d")
+      .createLinearGradient(0, 0, 0, 450);
+    this.gradient2.addColorStop(0, "rgba(0, 255,0, 0.5)");
+    this.gradient2.addColorStop(0.5, "rgba(0, 255, 0, 0.25)");
+    this.gradient2.addColorStop(1, "rgba(0, 0, 255, 0)");
     this.renderChart(
       {
         labels: Object.keys(this.chartdata),
@@ -36,6 +51,15 @@ export default {
             borderWidth: 1,
             pointBorderColor: "white",
             backgroundColor: this.gradient
+          },
+          {
+            label: this.titleaux,
+            data: Object.values(this.chartdataaux),
+            borderColor: "#00CC00",
+            pointBackgroundColor: "black",
+            borderWidth: 1,
+            pointBorderColor: "black",
+            backgroundColor: this.gradient2
           },
 
         ],
