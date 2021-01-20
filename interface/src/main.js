88c7a11885c2bd2637147a2ecded7a044b48946d
Luís Braga <a82088@alunos.uminho.pt>
2021-01-20 14:46:27 +0000

    uninstall chat

--- interface/src/main.js
+++ interface/src/main.js
@@ -2,13 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
-import Chat from 'vue-beautiful-chat'
 
 
 Vue.config.devtools = false;
 Vue.config.productionTip = false;
-
-Vue.use(Chat)
 
 new Vue({
   router,
